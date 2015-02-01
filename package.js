adabeb0d622c30febbdfb55612017089ec24cef4
peter.pavlovich <peter.pavlovich@enernoc.com>
2015-02-01 00:00:45 -0500

    initial commit of new branch

--- package.js
+++ package.js
@@ -1,16 +1,21 @@
 Package.describe({
-  summary: 'Routing specifically designed for Meteor'
+    name: 'sgi:iron-router',
+  version: '0.0.1',
+  summary: 'Routing specifically designed for Meteor',
+  git: 'http://github.com/sginc/iron-router',
+  documentation: 'README.md'
 });
 
 Package.on_use(function (api) {
-  api.use('reactive-dict', ['client', 'server']);
-  api.use('deps', ['client', 'server']);
-  api.use('underscore', ['client', 'server']);
-  api.use('ejson', ['client', 'server']);
+  api.versionsFrom('METEOR@1.0.3')
+  api.use('reactive-dict');
+  api.use('deps');
+  api.use('underscore');
+  api.use('ejson');
   api.use('jquery', 'client');
 
   // default ui manager
-  // use unordered: true becuase of circular dependency
+  // use unordered: true because of circular dependency
 
   // for helpers
   api.use('ui', 'client');
@@ -22,54 +27,54 @@ Package.on_use(function (api) {
   // package automatically when users add iron-router.
   api.use('blaze-layout', 'client', {unordered: true});
 
-  api.add_files('lib/utils.js', ['client', 'server']);
-  api.add_files('lib/route.js', ['client', 'server']);
-  api.add_files('lib/route_controller.js', ['client', 'server']);
-  api.add_files('lib/router.js', ['client', 'server']);
+  api.addFiles('lib/utils.js');
+  api.addFiles('lib/route.js');
+  api.addFiles('lib/route_controller.js');
+  api.addFiles('lib/router.js');
 
-  api.add_files('lib/client/location.js', 'client');
-  api.add_files('lib/client/router.js', 'client');
-  api.add_files('lib/client/wait_list.js', 'client');
-  api.add_files('lib/client/hooks.js', 'client');
-  api.add_files('lib/client/route_controller.js', 'client');
-  api.add_files('lib/client/ui/helpers.js', 'client');
+  api.addFiles('lib/client/location.js', 'client');
+  api.addFiles('lib/client/router.js', 'client');
+  api.addFiles('lib/client/wait_list.js', 'client');
+  api.addFiles('lib/client/hooks.js', 'client');
+  api.addFiles('lib/client/route_controller.js', 'client');
+  api.addFiles('lib/client/ui/helpers.js', 'client');
 
-  api.add_files('lib/server/route_controller.js', 'server');
-  api.add_files('lib/server/router.js', 'server');
+  api.addFiles('lib/server/route_controller.js', 'server');
+  api.addFiles('lib/server/router.js', 'server');
 
   api.use('webapp', 'server');
   Npm.depends({connect: '2.9.0'});
 
-  api.export('RouteController', ['client', 'server']);
-  api.export('Route', ['client', 'server']);
-  api.export('Router', ['client', 'server']);
+  api.export('RouteController');
+  api.export('Route');
+  api.export('Router');
   api.export('IronLocation', 'client');
 
-  api.export('Utils', ['client', 'server'], {testOnly: true});
-  api.export('IronRouter', ['client', 'server'], {testOnly: true});
-  api.export('WaitList', 'client', {testOnly: true});
+  //api.export('Utils', ['client', 'server'], {testOnly: true});
+  //api.export('IronRouter', ['client', 'server'], {testOnly: true});
+  //api.export('WaitList', 'client', {testOnly: true});
 });
 
-Package.on_test(function (api) {
-  api.use('iron-router', ['client', 'server']);
-  api.use('tinytest', ['client', 'server']);
-  api.use('test-helpers', ['client', 'server']);
-  api.use('reactive-dict', ['client', 'server']);
-
-  api.add_files('test/test_helpers.js', ['client', 'server']);
-
-  // client and server
-  api.add_files('test/both/route.js', ['client', 'server']);
-  api.add_files('test/both/route_controller.js', ['client', 'server']);
-  api.add_files('test/both/router.js', ['client', 'server']);
-  api.add_files('test/both/utils.js', ['client', 'server']);
-
-  // server only
-  api.add_files('test/server/router.js', 'server');
-
-  // client only
-  api.add_files('test/client/mocks.js', 'client');
-  api.add_files('test/client/router.js', 'client');
-  api.add_files('test/client/route_controller.js', 'client');
-  api.add_files('test/client/wait_list.js', 'client');
-});
+//Package.on_test(function (api) {
+//  api.use('iron-router', ['client', 'server']);
+//  api.use('tinytest', ['client', 'server']);
+//  api.use('test-helpers', ['client', 'server']);
+//  api.use('reactive-dict', ['client', 'server']);
+//
+//  api.add_files('test/test_helpers.js', ['client', 'server']);
+//
+//  // client and server
+//  api.add_files('test/both/route.js', ['client', 'server']);
+//  api.add_files('test/both/route_controller.js', ['client', 'server']);
+//  api.add_files('test/both/router.js', ['client', 'server']);
+//  api.add_files('test/both/utils.js', ['client', 'server']);
+//
+//  // server only
+//  api.add_files('test/server/router.js', 'server');
+//
+//  // client only
+//  api.add_files('test/client/mocks.js', 'client');
+//  api.add_files('test/client/router.js', 'client');
+//  api.add_files('test/client/route_controller.js', 'client');
+//  api.add_files('test/client/wait_list.js', 'client');
+//});
